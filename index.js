f0e0ecc369fc081cc5b9371c0b17dbb74543dd7a
Shubham Vijay Vargiy <svijay1692@gmail.com>
2017-05-08 13:17:57 +0530

    update

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 // parse application/json
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
+    //res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
    // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
