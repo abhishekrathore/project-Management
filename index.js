e7fe7e1907e16d8e6bd077ed86594862da7e4750
Shubham Vijay Vargiy <svijay1692@gmail.com>
2017-05-21 00:46:06 +0530

    update Index

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ app.use(cookieParser());
 // parse application/json
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    //res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-   // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 // here public is name of a folder of static file
@@ -56,8 +56,6 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        path: '/',
-        domain: 'https://obscure-springs-26671.herokuapp.com',
         maxAge: 1000 * 60 * 24 // 24 hours
     }
 }));
